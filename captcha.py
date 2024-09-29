b11ddc28c896350aebc9816c39235ab9fcb3b9b1
Parvez <rijwanoolkarim@gmail.com>
2024-09-29 21:13:10 +0530

    captcha file added

--- captcha.py
+++ captcha.py
@@ -1,26 +1,20 @@
 import string
-import random
 from captcha.image import ImageCaptcha
 from PIL import Image
 import random
 
-# Function to generate a random CAPTCHA text of specified length
-def captcha_image_text(length=10):
-    return "".join(random.choices(string.ascii_letters + string.digits, k=length))
+def captcha_image_text(lenght=10):
+    return " ".join(random.choices(string.ascii_letters + string.digits, k=lenght))
 
-# Function to generate and save CAPTCHA image
-def captcha_image(captcha_text, image_width=300):
+def captcha_image(captcha_text,image_width=300):
     image = ImageCaptcha(image_width)
     image_file = f"{captcha_text}.png"
-    image.write(captcha_text, image_file)
+    image.write(captcha_text,image_file)
     return image_file
 
-# Generate random CAPTCHA text
-captcha_text = captcha_image_text()
 
-# Create and save the CAPTCHA image
+captcha_text = captcha_image_text()
 image_file = captcha_image(captcha_text=captcha_text)
 
-# Output message and open the generated CAPTCHA image
-print(f"Generated CAPTCHA: {captcha_text}")
-Image.open(image_file).show()  # Use .show() to open the image
+print(f"Generated Captcha")
+Image.open(image_file)
